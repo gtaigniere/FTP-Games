156003b4b3271a6284a1457fc1281358ebcdaf2f
gillout <54893913+gillout@users.noreply.github.com>
2023-01-25 10:44:40 +0100

    Ajout d'un tableau d'utilisateurs

--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -2,3 +2,18 @@ export interface User {
   email: string;
   password: string;
 }
+
+export const users = [
+  {
+    email: 'gillout@bbox.fr',
+    password: 'gillout1'
+  },
+  {
+    email: 'patrick@free.fr',
+    password: 'patrick2'
+  },
+  {
+    email: 'jeffrey@sfr.fr',
+    password: 'jeffrey3'
+  }
+];
